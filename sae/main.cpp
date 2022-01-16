609c5bdbe60b90949c9530e625434d0d724be446
Aurelien Habermacher <aurelienhabermacher@gmail.com>
2022-01-16 10:36:15 +0100

    init bambou

--- sae/main.cpp
+++ sae/main.cpp
@@ -6,6 +6,15 @@
 #include "update.cpp"
 using namespace std;
 
+void init_bambou(Bambou tabbambou[NBBAMBOUX][NBBAMBOUY], int taillex, int tailley) {
+    for (int i1 = 0; i1 < taillex; i1++) {
+        for (int i2 = 0; i2 < tailley; i2++) {
+            tabbambou[i1][i2].taille = 1;
+            tabbambou[i1][i2].vitesse = 1;
+        }
+    }
+}
+
 int debug_stat(Bambou tabbambou[NBBAMBOUX][NBBAMBOUY], int taillex, int tailley) {
     return 0;
 }
@@ -19,5 +28,6 @@ int main()
 {
     Panda Pand1;
     Bambou tabbambou[NBBAMBOUX][NBBAMBOUY];
+    init_bambou(tabbambou, NBBAMBOUX, NBBAMBOUY);
     cout << debug_stat(tabbambou, 50, 50);
 }
