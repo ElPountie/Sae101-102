eb956dd2ed29d2dd0123b41d5cc594ad7a4ea50d
Thibault Defour <thibault.defour@etu.univ-lyon1.fr>
2022-01-17 10:39:57 +0100

    Suppression main

--- sae/main.cpp
+++ sae/main.cpp
@@ -5,17 +5,5 @@ using namespace std;
 
 
 int main() {
-	int bambou[nb_bambou][nb_bambou];
-	int croissance[nb_bambou][nb_bambou];
-	init_bambouseraie(bambou, nb_bambou);
-	vitesse_croissance(croissance, nb_bambou);
-	croissance_bambouseraie(bambou,croissance, nb_bambou);
-	for (int i = 0; i < nb_bambou; i++) {
-		for (int j = 0; j < nb_bambou; j++) {
-			cout << bambou[i][j];
-		}
-		cout << endl;
-	}
-	cout << croissance_max(bambou, croissance, nb_bambou);
 	return 0;
 }
