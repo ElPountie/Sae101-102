202d9677c425eb828ba1838b98e16137287320ca
Thibault Defour <thibault.defour@etu.univ-lyon1.fr>
2022-01-17 09:23:06 +0100

    Amélioration croissance rajout fonction vitesse_croissance

--- sae/main.cpp
+++ sae/main.cpp
@@ -14,18 +14,28 @@ void init_bambouseraie(int bambou[][nb_bambou], int nb_bambou) {
 	}
 }
 
-void croissance_bambouseraie(int bambou[][nb_bambou], int nb_bambou) {
+void vitesse_croissance(int croissance[][nb_bambou], int nb_bambou) {
 	for (int i = 0; i < nb_bambou; i++) {
 		for (int j = 0; j < nb_bambou; j++) {
-			bambou[i][j] = bambou[i][j] + rand() % 5 * h_start_bambou;
+			croissance[i][j] = rand() % 5;
+		}
+	}
+}
+
+void croissance_bambouseraie(int bambou[][nb_bambou], int croissance[][nb_bambou], int nb_bambou) {
+	for (int i = 0; i < nb_bambou; i++) {
+		for (int j = 0; j < nb_bambou; j++) {
+			bambou[i][j] = bambou[i][j] + croissance[i][j] * h_start_bambou;
 		}
 	}
 }
 
 int main() {
 	int bambou[nb_bambou][nb_bambou];
+	int croissance[nb_bambou][nb_bambou];
 	init_bambouseraie(bambou, nb_bambou);
-	croissance_bambouseraie(bambou, nb_bambou);
+	vitesse_croissance(croissance, nb_bambou);
+	croissance_bambouseraie(bambou,croissance, nb_bambou);
 	for (int i = 0; i < nb_bambou; i++) {
 		for (int j = 0; j < nb_bambou; j++) {
 			cout << bambou[i][j];
