f518ba8d72a620037944ce5dbae064431253b31d
Aurélien Habermacher <aurelienhabermacher@gmail.com>
2022-01-18 09:52:43 +0100

    Patch movment

--- sae/func_khalis.h
+++ sae/func_khalis.h
@@ -6,10 +6,12 @@
 #include "config_sdl.h"
 #include "Bambou.h"
 #include "Constante_Thibault.h"
+#include "Panda.h"
 
 void carre(SDL_Renderer* rendu,int nb_carre);
 void place_img(SDL_Texture* monImage, SDL_Rect posImg, SDL_Renderer* rendu);
 void ecrit(SDL_Renderer* rendu, TTF_Font* font);
 void affiche_bambou(SDL_Renderer* rendu,Bambou tab[sqrt_nb_bambou][sqrt_nb_bambou],int nb_carre);
 int init(Bambou tab[sqrt_nb_bambou][sqrt_nb_bambou],int nb_cote);
+void update_movment(SDL_Rect posImg, Panda panda, SDL_Renderer* rendu, TTF_Font* font, Bambou tab[][sqrt_nb_bambou], SDL_Texture* monImage, int& nb_cote, int direction);
 #endif
